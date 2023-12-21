bac962624a8c4a9f212326b1f9dafec52fea153a
IspahanRahman <ispahanrahman133@gmail.com>
2023-12-21 10:07:51 +0600

    addCourse and getAllCoursesapi implemented

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -21,10 +21,59 @@ const login = async (req,res) =>{
     })
 }
 
+const getAllCourses = (req, res) => {
+    try{
+        const q = "SELECT * FROM course";
+        conn.query(q, (err, result) => {
+            if(err) {
+                throw err;
+            }
+            return res.status(200).json(result);
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+}
+const addCourse = (req,res)=>{
+    try{
+        const {name,description,price,duration,level,topics,schedule} = req.body;
+        const q = "INSERT INTO course (name,description,price,duration,level,topics,schedule) VALUES(?,?,?,?,?,?,?)";
+        conn.query(q,[name,description,price,duration,level,topics,schedule],(err, result) =>{
+            if(err) {
+                throw err;
+            }
+            return res.status(200).json({
+                message: "The course has been added successfully"
+            });
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+}
+
+
+const deleteCourse = (req,res)=>{
+    try{
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+}
 
 
 
 module.exports = {
     login,
-
+    getAllCourses,
+    addCourse,
+    deleteCourse
 }
--- routers/adminRoute.js
+++ routers/adminRoute.js
@@ -2,9 +2,13 @@ const adminRoute = require('express').Router();
 
 const {
     login,
+    getAllCourses,
+    addCourse
 }=require('../controllers/adminController');
 
 adminRoute.post('/login',login);
+adminRoute.get('/allCourses',getAllCourses);
+adminRoute.post('/addCourse',addCourse);
 
 
 module.exports = adminRoute;
